af8c1eb9132c375432e2c6f827beb93134c10741
Marco Antonio <94036718+MKoshin@users.noreply.github.com>
2025-04-14 13:43:30 -0300

    Update xadrez.c
    Cavalo update!

--- xadrez.c
+++ xadrez.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     // bispo 5 casas diagonal
     // rainha 8 casas a esquerda
     // torre 5 casas a frente
 
     int opcao, movimento; // variavel pra receber as opções switch
 
-    int b = 0; // variavel bispo
-    int i = 0; // variavel torre
+    int b; // variavel bispo
+    int i; // variavel torre
 
     printf("Opções\n");
     printf("1. Bispo\n");
     printf("2. Rainha\n");
     printf("3. Torre\n");
+    printf("4. Cavalo\n");
     printf("Escolha uma opção: ");
     scanf("%d", &opcao);
 
-    switch(opcao){
-    
-    case 1:
+    switch (opcao)
+    {
+
+    case 1: // MOVIMENTO BISPO
+
         printf("Escolha de movimentos\n");
         printf("1. Diagonal superior direita\n");
         printf("2. Diagonal superior esquerda\n");
@@ -30,51 +34,58 @@ int main(){
 
         switch (movimento)
         {
-        case 1:
+        case 1: // MOVIMENTO DIAGONAL BISPO SUPERIOR DIREITA
 
-            while(b < 5){
-            printf("Direita, ");
-            printf("Cima\n");
-            b++;
+            while (b < 5)
+            {
+                printf("Direita, ");
+                printf("Cima\n");
+                b++;
             }
 
             break;
-        case 2:
 
-            while (b < 5){
-            printf("Esquerda, ");
-            printf("Cima\n");
-            b++;
+        case 2: // MOVIMENTO DIAGONAL BISPO SUPERIOR ESQUERDA
+
+            while (b < 5)
+            {
+                printf("Esquerda, ");
+                printf("Cima\n");
+                b++;
             }
-            
 
             break;
-        case 3:
 
-            while (b < 5){
-            printf("Direita, ");
-            printf("Baixo\n");
-            b++;
+        case 3: // MOVIMENTO DIAGONAL BISPO INFERIOR DIREITA
+
+            while (b < 5)
+            {
+                printf("Direita, ");
+                printf("Baixo\n");
+                b++;
             }
-            
+
             break;
 
-        case 4:
+        case 4: // MOVIMENTO DIAGONAL BISPO INFERIOR ESQUERDA
 
-            while (b < 5){
-            printf("Esquerda, ");
-            printf("Baixo\n");
-            b++;
-            } 
+            while (b < 5)
+            {
+                printf("Esquerda, ");
+                printf("Baixo\n");
+                b++;
+            }
+
             break;
-        
+
         default:
-        printf("Opção errada");
-
+            printf("Opção errada");
         }
+
         break;
 
-    case 2:
+    case 2: // MOVIMENTO DA RAINHA
+
         printf("Escolha de movimentos\n");
         printf("1. Direita\n");
         printf("2. Esquerda\n");
@@ -87,66 +98,94 @@ int main(){
         printf("Escolha o movimento: ");
         scanf("%d", &movimento);
 
-        switch(movimento){
+        switch (movimento)
+        {
 
-            case 1:
-                for (int r = 0; r < 8; r++){
-                    printf("Direita");
-                }
+        case 1: // MOVIMENTO RAINHA DIREITA
+
+            for (int r = 0; r < 8; r++)
+            {
+                printf("Direita");
+            }
+
             break;
 
-            case 2:
-                for (int r = 0; r < 8; r++){
-                    printf("Esquerda");
-                }
+        case 2: // MOVIMENTO RAINHA ESQUERDA
+
+            for (int r = 0; r < 8; r++)
+            {
+                printf("Esquerda");
+            }
+
             break;
 
-            case 3:
-                for (int r = 0; r < 8; r++){
-                    printf("Cima");
-                }
+        case 3: // MOVIMENTO RAINHA CIMA
+
+            for (int r = 0; r < 8; r++)
+            {
+                printf("Cima");
+            }
+
             break;
 
-            case 4:
-                for (int r = 0; r < 8; r++){
-                    printf("Baixo");
-                }
+        case 4: // MOVIMENTO RAINHA BAIXO
+
+            for (int r = 0; r < 8; r++)
+            {
+                printf("Baixo");
+            }
+
             break;
 
-            case 5:
-                for (int r = 0; r < 8; r++){
-                    printf("Direita");
-                    printf("Cima\n");
-                }
+        case 5: // MOVIMENTO DIAGONAL SUPERIOR DIREITA
+
+            for (int r = 0; r < 8; r++)
+            {
+                printf("Direita");
+                printf("Cima\n");
+            }
+
             break;
 
-            case 6:
-                for (int r = 0; r < 8; r++){
-                    printf("Esquerda");
-                    printf("Cima\n");
-                }
+        case 6: // MOVIMENTO DIAGONAL SUPERIOR ESQUERDA
+
+            for (int r = 0; r < 8; r++)
+            {
+                printf("Esquerda");
+                printf("Cima\n");
+            }
+
             break;
 
-            case 7:
-                for (int r = 0; r < 8; r++){
-                    printf("Direita");
-                    printf("Baixo\n");
-                }
+        case 7: // MOVIMENTO DIAGONAL INFERIOR DIREITA
+
+            for (int r = 0; r < 8; r++)
+            {
+                printf("Direita");
+                printf("Baixo\n");
+            }
+
             break;
 
-            case 8:
-                for (int r = 0; r < 8; r++){
-                    printf("Esquerda");
-                    printf("Baixo\n");
-                }
+        case 8: // MOVIMENTO DIAGONAL INFERIOR BAIXO
+
+            for (int r = 0; r < 8; r++)
+            {
+                printf("Esquerda");
+                printf("Baixo\n");
+            }
+
             break;
-            default:
+
+        default:
+
             printf("Opção inválida");
+
         }
 
         break;
 
-    case 3: // movimento da torre
+    case 3: // MOVIMENTO DA TORRE
 
         printf("Escolha de movimentos\n");
         printf("1. Direita\n");
@@ -156,45 +195,144 @@ int main(){
         printf("Escolha o movimento: ");
         scanf("%d", &movimento);
 
-        switch(movimento){
+        switch (movimento)
+        {
 
-        case 1:
-            do{
+        case 1: // MOVIMENTO TORRE DIREITA
+
+            do
+            {
                 printf("Direita\n"); // imprime a direção do movimento
                 i++;
             } while (i < 5);
 
             break;
-        case 2:
-            do{
+
+        case 2: // MOVIMENTO TORRE ESQUERDA
+
+            do
+            {
                 printf("Esquerda\n"); // imprime a direção do movimento
                 i++;
             } while (i < 5);
 
             break;
 
-        case 3:
-            do{
+        case 3: // MOVIMENTO TORRE SUPERIOR
+
+            do
+            {
                 printf("Cima\n"); // imprime a direção do movimento
                 i++;
             } while (i < 5);
 
             break;
 
-        case 4:
-            do{
+        case 4: // MOVIMENTO TORRE INFERIOR
+
+            do
+            {
                 printf("Baixo\n"); // imprime a direção do movimento
                 i++;
             } while (i < 5);
             break;
 
         default:
-        printf("Opção inválida");
-}
-        break;
+            printf("Opção inválida");
+        }
+
+    case 4: // cavalo
+
+        printf("Escolha movimento\n");
+        printf("1. cima, direita\n");
+        printf("2. cima, esquerda\n");
+        printf("3. baixo, direita\n");
+        printf("4. baixo, esquerda\n");
+        scanf("%d", &movimento);
+
+        switch (movimento)
+        {
+
+        case 1: // MOVIMENTO CAVALO CIMA CIMA DIREITA
+
+            for (int c = 1; c <= 1; c++)
+            {
+                int a = 1;
+                do
+                {
+                    printf("Cima, direita\n");
+                    a++;
+                } while (a <= 1);
+
+                if (c >= 1)
+                {
+                    printf("Cima");
+                }
+            }
+
+            break;
+
+        case 2: // MOVIMENTO CAVALO CIMA CIMA ESQUERDA
+
+            for (int c = 1; c <= 1; c++)
+            {
+                int a = 1;
+                do
+                {
+                    printf("Esquerda Cima\n");
+                    a++;
+                } while (a <= 1);
+
+                if (c >= 1)
+                {
+                    printf("         Cima ");
+                }
+            }
+
+            break;
+
+        case 3: // MOVIMENTO CAVALO BAIXO BAIXO DIREITA
+
+            for (int c = 1; c <= 1; c++)
+            {
+                int a = 1;
+                do
+                {
+                    printf("Baixo\n");
+                    a++;
+                } while (a <= 1);
+
+                if (c >= 1)
+                {
+                    printf("Baixo, direita");
+                }
+            }
+
+            break;
+
+        case 4: // MOVIMENTO CAVALO BAIXO BAIXO ESQUERDA
+
+            for (int c = 1; c <= 1; c++)
+            {
+                int a = 1;
+                do
+                {
+                    printf("          Baixo\n");
+                    a++;
+                } while (a <= 1);
+
+                if (c >= 1)
+                {
+                    printf("Esquerda, Baixo");
+                }
+            }
+            
+            break;
 
         default:
-        printf("Opção inválida");
-}
-return 0;
+            printf("Opção inválida");
+        }
+    }
+
+    return 0;
 }
